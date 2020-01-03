3ca8289d1a0dd5a84607e0348f05d5d264e5aec8
SkitDev <melaine35gerard@gmail.com>
2020-01-03 18:03:26 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    browserSync = require("browser-sync").create();
+
+// compile scss into css
+
+function style(){
+    // 1. where is my scss file
+    return gulp.src('./scss/**/*.scss')
+    // 2. pass that file through sass compiler
+        .pipe(sass())
+    // 3. where do I save the compiled css
+        .pipe(gulp.dest('./css'))
+        // 4. stream changes to all browser
+        .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './',
+            notify: false
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+    gulp.watch('./**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
